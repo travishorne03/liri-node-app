69834dbe29040087928ff7ccb55872284d4f7dcd
Travis Horne <travis.horne03@gmail.com>
2017-10-30 20:06:10 -0400

    update

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'pB8eMkb2xoKP5gJag6xSe3elN',
+  consumer_secret: 'U0RIvMQuzdiOY8PF37KJ6WgD0oDfXRc4i0a7ItW2TXk2DYCbJT',
+  access_token_key: '72029520-v9YR1eJEx3oKyIEbmj53xCtDtPoVmCd2bw5Httjbn',
+  access_token_secret: 'EsOhXGveikqVef31kkPNdB5n5lfnsxluUCFx7LfJIxo9x',
+};
---
+++ liri.js
@@ -0,0 +1,68 @@
+var twitCred = require("./keys.js");
+
+//console.log(twitCred);
+
+var twitter = require("twitter");
+
+var request = require("request");
+
+//console.log(process.argv);
+
+var liriType = process.argv[2];
+var liriSearch= process.argv[3];
+
+var client = new twitter({
+    consumer_key: twitCred.twitterKeys.consumer_key,
+    consumer_secret: twitCred.twitterKeys.consumer_secret,
+    access_token_key: twitCred.twitterKeys.access_token_key,
+    access_token_secret: twitCred.twitterKeys.access_token_secret
+});
+
+switch (liriType) {
+        case 'Twitter':
+        tweets();
+        break;
+
+        case 'Movie':
+        movies();
+        break;
+
+        case 'Song':
+        songs();
+        break;
+}
+
+function tweets(){
+  console.log("twitter function");
+
+  var twitHandle = process.argv[3];
+	var params = { screen_name: liriSearch };
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	    if  (error)  {        
+	        console.log('Error occurred: '  +  error);      
+	        return;    
+	    }
+
+	    //console.log(tweets);
+      for (var i = 0; i < tweets.length; i++) {
+	        console.log('\r\n--------Tweet #' + (i + 1) + ' - Sent ' + tweets[i].created_at + '--------');
+	        console.log(tweets[i].text);
+	        console.log('----------------------------------------------------------------------');
+	        console.log(' ');
+	    }
+	    console.log('\r\n================================================================');
+
+	});
+}
+
+
+function movies(){
+  console.log("movie function");
+
+}
+
+function songs(){
+  console.log("songs function");
+
+}

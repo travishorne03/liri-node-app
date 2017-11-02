8ac8701d10a7fa591d5911d31610d3857109c88f
Travis Horne <travis.horne03@gmail.com>
2017-11-01 20:25:16 -0400

    update

--- keys.js
+++ keys.js
@@ -6,3 +6,10 @@ exports.twitterKeys = {
   access_token_key: '72029520-v9YR1eJEx3oKyIEbmj53xCtDtPoVmCd2bw5Httjbn',
   access_token_secret: 'EsOhXGveikqVef31kkPNdB5n5lfnsxluUCFx7LfJIxo9x',
 };
+
+
+exports.spotifyKeys = {
+  client ID: '0ab15ee48a3049228d0530eb1fec7316',
+  Client Secret: 'cdfb68150a7f4be380761c116c8176da',
+
+};
